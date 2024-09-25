a0e5c371e5150281897fe060fe4ea191826ca885
David <dsanzraya@hotmail.com>
2024-09-25 13:53:37 +0200

    protected person name

--- HospitalManagement/Person.cs
+++ HospitalManagement/Person.cs
@@ -8,7 +8,7 @@ namespace HospitalManagement
 {
     internal abstract class Person
     {
-        public string Name { get; set; }
+        public string Name { get; protected set; }
 
         public Person(string name)
         {
